2ef0a6963b0daa2e6925911439966236b2325b3b
Mohamad Kemal Rasyidin <kemalrasyidin@gmail.com>
2025-01-13 21:50:16 +0700

    Update aja

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import AppProvider from "./context/AppContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <AppProvider>
+    <App />
+  </AppProvider>
+);

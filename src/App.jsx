887befcfb4004970133a32342db2b624a7ea630a
Mohamad Kemal Rasyidin <kemalrasyidin@gmail.com>
2025-01-13 23:49:41 +0700

    Kelar dikit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppContext } from "./Context/AppContext";
 import "./App.css";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import { useContext } from "react";
-import { AppContext } from "./context/AppContext";
+import Create from "./pages/Posts/Create";
+import Show from "./pages/Posts/Show";
+import Update from "./pages/Posts/Update";
 
 export default function App() {
   const { user } = useContext(AppContext);
@@ -17,6 +20,9 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="/register" element={user ? <Home /> : <Register />} />
           <Route path="/login" element={user ? <Home /> : <Login />} />
+          <Route path="/create" element={user ? <Create /> : <Login />} />
+          <Route path="/posts/:id" element={<Show />} />
+          <Route path="/posts/update/:id" element={user ? <Update /> : <Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
